663aa25d0ad2f877ecf66ee6125a91fdc5ec0618
windecks <JOSEPH1231123104@gmail.com>
2025-01-26 13:28:16 -0500

    update html

--- experiment2/job2.sh
+++ experiment2/job2.sh
@@ -14,8 +14,8 @@ module load cudnn
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
-pip install --no-index $SCRATCH/nvidia_cutlass-3.6.0.0-py3-none-any.whl
-pip install --no-index -r $SCRATCH/requirements.txt
+pip install --no-index $SCRATCH/triton-matmul/nvidia_cutlass-3.6.0.0-py3-none-any.whl
+pip install --no-index -r $SCRATCH/triton-matmul/requirements.txt
 
 python experiment2.py
 
